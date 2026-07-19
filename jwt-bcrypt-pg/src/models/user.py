211e4421679708792e2ed77f111bb4ec933289c0
BhagabanGhadai <bhagabanghadai043@gmail.com>
2026-07-19 22:58:30 +0530

    feat: Enforce non-nullable constraints and uniqueness for user credentials in the database schema

--- jwt-bcrypt-pg/src/models/user.py
+++ jwt-bcrypt-pg/src/models/user.py
@@ -6,8 +6,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, index=True)
-    password = Column(String)
+    username = Column(String, index=True,nullable=False,unique=True)
+    password = Column(String,nullable=False)
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
     
